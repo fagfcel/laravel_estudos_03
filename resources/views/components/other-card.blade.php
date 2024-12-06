b672446b41b6432ae67b53a3acf2a51468dd79fa
Francisco Ferreira <estudosfagf@gmail.com>
2024-12-06 16:12:50 -0300

    008

---
+++ resources/views/components/other-card.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h3>Este é o texto inicial do componente</h3>
+        {{-- vamos exibir o conteúdo do slot aqui com a diretiva Blad --}}
+        {{ $slot }}
+
+    <h3>Este é o texto final do componente</h3>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.main_layout')
 @section('content')
-<div class="text-center">
-    @foreach ($pessoas_linguas as $pessoa => $linguas )
+{{-- <div class="text-center"> --}}
+    {{-- @foreach ($pessoas_linguas as $pessoa => $linguas ) --}}
         {{-- Component --}}
-        <x-card-pessoa :$pessoa :$linguas />
-    @endforeach
+        {{-- <x-card-pessoa :$pessoa :$linguas /> --}}
+    {{-- @endforeach --}}
+{{-- </div> --}}
+
+{{-- Other componete e $slot --}}
+<div>
+    <h4 class="text-info">Como funciona um Slot?</h4>
+    <x-other-card > 
+        <h1 class="text-danger">Este é o Slot</h1>
+    </x-other-card >
 </div>
 
 @endsection
