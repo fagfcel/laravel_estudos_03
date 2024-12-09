7a1032718fc13d8ec79a25b3ab7d91bcf176c915
Francisco Ferreira <estudosfagf@gmail.com>
2024-12-09 15:17:49 -0300

    009

---
+++ resources/views/components/multi-slot.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h4>Componente com vários slots</h4>
+    <p>{{ $title }} </p>
+    <p>{{ $content }} </p>
+    <p>{{ $footer }} </p>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,23 @@
 {{-- </div> --}}
 
 {{-- Other componete e $slot --}}
-<div>
+{{-- <div>
     <h4 class="text-info">Como funciona um Slot?</h4>
     <x-other-card > 
         <h1 class="text-danger">Este é o Slot</h1>
     </x-other-card >
-</div>
+</div> --}}
+
+<x-multi-slot>
+    <x-slot:title>Este é o titulo!!</x-slot:title>
+    <x-slot:content>Este é o contexto!!</x-slot:content>
+    <x-slot:footer>
+        <ul>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+        </ul>
+    </x-slot:footer>
+</x-multi-slot>
 
 @endsection
